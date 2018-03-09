f94bc1dc5bb740cb6e083c478769f38a0ab321ea
Ripal Patel <patelripal.job@gmail.com>
2018-03-09 00:03:22 -0600

    Add project files.

---
+++ MyFirstMVC/Controllers/PeopleController.cs
@@ -0,0 +1,28 @@
+﻿using MyFirstMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstMVC.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: People
+        public ActionResult ListPeople()
+        {
+            List<PersonModel> people = new List<PersonModel>();
+            people.Add(new PersonModel { FirstName = "Ripal", LastName = "Patel", Age = 30 });
+            people.Add(new PersonModel { FirstName = "Tejas", LastName = "Patel", Age = 32 });
+            people.Add(new PersonModel { FirstName = "Viaan", LastName = "Patel", Age = 2 });
+            return View(people);
+        }
+    }
+}
---
+++ MyFirstMVC/Models/PersonModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyFirstMVC.Models
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool Isalive { get; set; } = true;
+        public int Age { get; set; } = 0;
+
+    }
+}
